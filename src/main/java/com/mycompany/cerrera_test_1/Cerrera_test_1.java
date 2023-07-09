3391a4dcc6c5842db9bdd47413f64575797abc1c
Daviferlo12 <dflopez345@misena.edu.co>
2023-07-09 15:11:27 -0500

    Fisrt Commit

---
+++ src/main/java/com/mycompany/cerrera_test_1/Cerrera_test_1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cerrera_test_1;
+
+/**
+ *
+ * @author davif
+ */
+public class Cerrera_test_1 {
+
+    public static void main(String[] args) {
+       form_corredor corredor = new form_corredor();
+       corredor.setVisible(true);
+    }
+}
---
+++ src/main/java/com/mycompany/cerrera_test_1/c_conexion.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cerrera_test_1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author davif
+ */
+public class c_conexion {
+    
+    Connection conectar = null;
+    
+    String user = "root";
+    String pass = "admin123";
+    String db = "carrera";
+    String ip = "localhost";
+    String port = "3306";
+    
+    
+    String chain = "jdbc:mysql://"+ ip + ":"+port+"/" + db;
+    
+    
+    public Connection establishConnection(){
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conectar = DriverManager.getConnection(chain, user, pass);
+            JOptionPane.showMessageDialog(null, "La conneccion se he realizado con exio.");
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos, error: " + e.toString());
+        }
+        
+        return conectar;
+    }
+
+
+
+}
