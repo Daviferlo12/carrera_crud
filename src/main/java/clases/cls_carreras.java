4f7d9f2847310d4ff5d8ff522ec16696661b87fc
Daviferlo12 <dflopez345@misena.edu.co>
2023-07-11 09:08:27 -0500

    cls_carrera

--- src/main/java/clases/cls_carreras.java
+++ src/main/java/clases/cls_carreras.java
@@ -10,4 +10,22 @@ package clases;
  */
 public class cls_carreras {
     
+    String nombre;
+    int id_etapa;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId_etapa() {
+        return id_etapa;
+    }
+
+    public void setId_etapa(int id_etapa) {
+        this.id_etapa = id_etapa;
+    }
 }
