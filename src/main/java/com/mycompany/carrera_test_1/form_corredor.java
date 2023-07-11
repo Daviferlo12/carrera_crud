402ef1f31dfb4b4be6702ce966cd87d7b0397410
Daviferlo12 <dflopez345@misena.edu.co>
2023-07-11 12:29:09 -0500

    XML ERROR

--- src/main/java/com/mycompany/carrera_test_1/form_corredor.java
+++ src/main/java/com/mycompany/carrera_test_1/form_corredor.java
@@ -6,10 +6,7 @@ import conection.c_conexion;
 import java.sql.Connection;
 import javax.swing.JComboBox;
 
-/**
- *
- * @author davif
- */
+
 public class form_corredor extends javax.swing.JFrame {
     
     // INSTANCIA DE CONEXION
