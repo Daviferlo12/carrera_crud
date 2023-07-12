c557d93a6f1a4508dc3f0b821447b1585aca568a
Daviferlo12 <dflopez345@misena.edu.co>
2023-07-11 23:15:39 -0500

    --

--- src/main/java/com/mycompany/carrera_test_1/Carrera_test_1.java
+++ src/main/java/com/mycompany/carrera_test_1/Carrera_test_1.java
@@ -1,16 +1,6 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.carrera_test_1;
 
-/**
- *
- * @author davif
- */
 public class Carrera_test_1 {
-
     public static void main(String[] args) {
        index inicio = new index();
        inicio.setVisible(true);
